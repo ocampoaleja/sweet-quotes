ea2c6048fa4451a5d75a27201277aa773958b4f4
ocampoaleja <omaira.527@hotmail.com>
2020-07-06 18:38:43 -0500

    Updates

---
+++ precache-manifest.2530d0557bd24792e7b1a62d170c927d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d57ec16c4ab12c3eb63a7e23cb48f7e",
+    "url": "/sweet-quotes/index.html"
+  },
+  {
+    "revision": "10123fce22fcc0fc5953",
+    "url": "/sweet-quotes/static/css/main.ae5b7ad7.chunk.css"
+  },
+  {
+    "revision": "ceb539af8f51a187dba4",
+    "url": "/sweet-quotes/static/js/2.c7732698.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/sweet-quotes/static/js/2.c7732698.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10123fce22fcc0fc5953",
+    "url": "/sweet-quotes/static/js/main.e5e6d5fe.chunk.js"
+  },
+  {
+    "revision": "7ed7f0f3bfcab6cd5848",
+    "url": "/sweet-quotes/static/js/runtime-main.81168549.js"
+  }
+]);
